a88fcc30e0656060ddfff9507e1f9c65fb5698d2
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-06-13 01:29:39 -0700

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+mkdir $HOME/projdir
+mkdir $HOME/usrprojdir
+
+export WM_PROJECT_NAME=hyStrathProject
+export WM_PROJECT_DIR=$HOME/projdir
+export WM_PROJECT_USER_DIR=$HOME/usrprojdir
+
  sudo apt-get install g++-7 gcc-7 -y
  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 7 -y 
  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 7 -y 
