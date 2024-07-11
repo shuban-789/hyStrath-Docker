3779cac28cba6d0843a63a6b6f1a99a0d18da589
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-07-11 00:39:35 -0700

    Update FinalSetup.sh

--- FinalSetup.sh
+++ FinalSetup.sh
@@ -27,3 +27,48 @@ cd OpenFOAM-v1706
 source ./etc/bashrc
 ./Allwmake
 
+echo "=== Cloning hyStrath ==="
+cd $WM_PROJECT_USER_DIR
+git clone https://github.com/hystrath/hyStrath.git --branch master --single-branch && cd hyStrath/
+
+echo "=== ALERT ==="
+
+function partition() {
+  size=$1
+  echo "tmpfs /run tmpfs defaults,size=$size 0 0" >> /etc/fstab
+  sudo mount -o remount /run
+}
+
+function prompt() {
+  echo -n "Partition? (yY/nN) "
+  read opt
+  if [[ $opt == "Y" ] || [ $opt == "y" ]]; then {
+    echo -n "Size? (e.g. 1G/1M/1K/auto) "
+    read opt2
+    if [ $opt2 == "auto" ]; then {
+      partition "1G"
+    } else {
+      echo -n "You want to give /run a size of $opt2? (yY/nN) "
+      read opt3
+      if [[ $opt3 == "Y" ] || [ $opt3 == "y" ]]; then {
+        partition "$opt2"
+      } else {
+        echo "Auto partitioning..."
+        partition "1G"
+      } fi
+    } fi
+  } elif [[ $opt == "N" ] || [ $opt == "n" ]]; then {
+    echo "Continuing..."
+  } else {
+    prompt
+  } fi
+}
+
+echo "Before installing the modules, please confirm that the /run filesystem is not full."
+echo "-----------------------------------------------------------------------------------"
+du -h
+echo "-----------------------------------------------------------------------------------"
+prompt
+
+
+echo "=== Installing Modules ==="
