40ba13d48cb88982a54596815670db600ca5b881
shuban-789 <cybergeek11929@gmail.com>
2024-07-25 11:08:04 -0700

    dependency instalation is interactive

--- build.sh
+++ build.sh
@@ -4,15 +4,16 @@
 
 function dependency_installation() {
     echo "=== Installing Dependencies ==="
+    export DEBIAN_FRONTEND=noninteractive
     apt-get update
-    apt-get install g++-7 gcc-7 -y
-    apt-get install software-properties-common -y
+    apt-get install g++-7 gcc-7 -yq
+    apt-get install software-properties-common -yq
     add-apt-repository ppa:rock-core/qt4
     apt-get update
-    apt-get install build-essential flex bison cmake zlib1g-dev libboost-system-dev libboost-thread-dev libopenmpi-dev openmpi-bin gnuplot libreadline-dev libncurses-dev libxt-dev -y
-    apt-get install qt4-dev-tools libqt4-dev libqt4-opengl-dev freeglut3-dev libqtwebkit-dev -y
-    apt-get install libscotch-dev libcgal-dev -y
-    apt-get install wget git -y
+    apt-get install build-essential flex bison cmake zlib1g-dev libboost-system-dev libboost-thread-dev libopenmpi-dev openmpi-bin gnuplot libreadline-dev libncurses-dev libxt-dev -yq
+    apt-get install qt4-dev-tools libqt4-dev libqt4-opengl-dev freeglut3-dev libqtwebkit-dev -yq
+    apt-get install libscotch-dev libcgal-dev -yq
+    apt-get install wget git -yq
     update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 7
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 7
     echo " "
