0c1a37b04bed9323402b1c97669ccacae0d93542
shuban-789 <cybergeek11929@gmail.com>
2024-07-25 01:41:26 -0700

    docker command script

--- docker.sh
+++ docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Description: This script is to build docker image and run software on the container using the recipe from the Dockerfile
-
+echo "========= Docker Images ========="
 echo " "
 sudo docker images
+echo " "
+echo "========= Docker Containers ========="
 echo " "
 sudo docker ls
 echo " "
