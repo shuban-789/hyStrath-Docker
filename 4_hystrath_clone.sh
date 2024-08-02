f0a6db389550027681a2324c82eec4d75a940702
shuban-789 <cybergeek11929@gmail.com>
2024-08-02 01:49:09 -0700

    streamlined installation,  cloning happens from specific branch version, bashrc is sourced frequently. User is exported to root to avoid empty USER env variable.

--- 4_hystrath_clone.sh
+++ 4_hystrath_clone.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 set -e
+set -x
 
-echo "=== Cloning hyStrath ==="
-cd $WM_PROJECT_USER_DIR
-git clone https://github.com/hystrath/hyStrath.git --branch master --single-branch && cd hyStrath/
-echo " "
+export USER=root
+source /root/OpenFOAM-v1706/etc/bashrc
+mkdir -p "$WM_PROJECT_USER_DIR"
+cd "$WM_PROJECT_USER_DIR"
+echo "=== Cloning hyStrath in $PWD ==="
+git init hyStrath
+cd hyStrath
+git remote add origin https://github.com/hystrath/hyStrath.git
+COMMIT_HASH='984e3000a5f8d2e47cee555ec466d58ea9a6315d'
+git fetch --depth 1 origin "$COMMIT_HASH"
+git branch master "$COMMIT_HASH"
+git checkout
--- 6_module_installation.sh
+++ 6_module_installation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 set -e
+set -x
 
 echo "=== Installing Modules ==="
 source /root/OpenFOAM-v1706/etc/bashrc
+cd "$WM_PROJECT_USER_DIR/hyStrath"
 export PATH="/root/OpenFOAM-v1706/wmake:$PATH"
-printf '4' | ./install.sh NUMPROCS
-echo " "
+./build/install-CFD.sh "$(nproc)"
+./build/install-DSMC.sh "$(nproc)"
+./build/install-hybridPICDSMC.sh "$(nproc)"
